2e605c7e1dbf83b0d73283ccd214f3ffea2612e7
Alexander Panin <panin1970@gmail.com>
2024-12-19 08:14:02 +0300

    Initial Commit

---
+++ MagicStick/MagicStickApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicStickApp.swift
+//  MagicStick
+//
+//  Created by Александр Панин on 19.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MagicStickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
